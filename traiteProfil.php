a9bf9e67a815fdab9aad404010cb0ae5164ad9cc
pelelo <epelle911e@ensc.fr>
2016-05-25 12:01:56 +0200

    Update traiteProfil.php

--- traiteProfil.php
+++ traiteProfil.php
@@ -1,51 +1,63 @@
 <?php
+ session_start();
  include "head.php";  
 
+ require("connexionbase.php");
 
  $resprinc = $_POST['residenceprincipale'];
  $ressecond = $_POST['residencesecondaire'];
  $logvac = $_POST['logvac'];
  $peche = $_POST['peche'];
-	  
 
-	if ($resprinc != " " && $peche == "oui")
+ echo $_SESSION['idparticipant'];
+ echo $resprinc;	  
+
+	if ($resprinc != "1" && $peche == "oui")
 	{
 		$_SESSION['profil'] = "ResPrincipalePecheur";
+		?><div><center><a href="partie1resident.php" class="button2">partie1pecheur</a></center></div><?php
 	}
-	if ($peche == "oui" && $ressecond != " ")
+	elseif ($peche == "oui" && $ressecond != "1")
 	{
-		$_SESSION['profil'] = "Res2dairePecheur";	
+		$_SESSION['profil'] = "Res2dairePecheur";
+		?><div><center><a href="partie1resident.php" class="button2">partie1pecheur</a></center></div><?php	
 	}
-	if ($ressecond = " " && $resprinc = " " && $peche == "oui")
+	elseif ($ressecond == "1" && $resprinc == "1" && $peche == "oui")
 	{
 		$_SESSION['profil'] = "VacancierPecheur";
+		?><div><center><a href="partie1pecheur.php" class="button2">partie1pecheur</a></center></div><?php
 	}
-	if ($resprinc != " " && $peche == "non")
+	elseif ($resprinc != "1" && $peche == "non")
 	{
 		$_SESSION['profil'] = "ResPrincipaleNonpecheur";
+		?><div><center><a href="partie1resident.php" class="button2">partie1resident</a></center></div><?php
 	}
-	if ($ressecond != " " && $peche == "non") 
+	elseif ($ressecond != "1" && $peche == "non") 
 	{
 		$_SESSION['profil'] = "Res2daireNonpecheur";
+		?><div><center><a href="partie1resident.php" class="button2">partie1resident</a></center></div><?php
 	}
-	if ($ressecond = " " && $resprinc = " " && $peche == "oui")
+	elseif ($ressecond == "1" && $resprinc == "1" && $peche == "non")
 	{
 		$_SESSION['profil'] = "VacancierNonPecheur";
+		?><div><center><a href="partie1vacancier.php" class="button2">partie1vacancier </a></center></div><?php
 	}
 
 $profil = $_SESSION['profil'];
 $id = $_SESSION['idparticipant'];
 
-echo $profil;
-echo $id;
-
-$requete ="UPDATE introduction SET TypeProfil = $profil WHERE IDparticipant = $id";
+$requete = "UPDATE introduction SET TypeProfil = '$profil' WHERE IDparticipant = '$id'";
 mysql_query($requete,$CONNEXION);
 
+echo $profil;
+
 ?>
-<div class="button">
-				<center><button type="submit">Valider</button></center>
-</div>
+
+
+
+
+
+
 
 
 
