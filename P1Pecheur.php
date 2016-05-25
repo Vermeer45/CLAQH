ca32a799bf620799bed720be1a107f7125bfd21a
Vermeer45 <vmeisterman@ensc.fr>
2016-05-25 15:28:43 +0200

    Update P1Pecheur.php

--- P1Pecheur.php
+++ P1Pecheur.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-        <title>Formulaire participant CLAQH</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <link rel="stylesheet" type="text/css" href="defaut.css"/>
-    </head>
+<?php
+include "head.php";  
+?>
 
 <body>
 		<center><h1>Partie 1 - Pécheur</h1></center>
